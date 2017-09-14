0d5d13c5d203904448a85e69bf39d040c12bc772
skatt931 <i.kurnytskyi@gmail.com>
2017-09-14 15:52:51 +0300

    four blocks are done. need to add some info and some other small things to change.

--- app/js/common.js
+++ app/js/common.js
@@ -5,7 +5,10 @@ $(function() {
 		$(window).on("scroll", function (e) {
 			var scroled = $(window).scrollTop();
 
-			$("#first-block").css("left", (0 - (scroled*1)) + "px");
+			$("#first-block").css("left", (0 - (scroled*0.1)) + "%");
+			$("#second-block").css("left", (70 - (scroled*0.1)) + "%");
+			$("#third-block").css("left", (120 - (scroled*0.1)) + "%");
+			$("#fourth-block").css("left", (190 - (scroled*0.1)) + "%");
 
 			$("body").css("background-position-x",(100 + (scroled * 0.25)) + "%");
 			//$("#super_bg").css("left", (700 + (scroled*0.25)) + "px");
