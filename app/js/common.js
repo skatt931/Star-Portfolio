7a374909ad8eb5eb2ab9cfc50df65bf5136ddde1
skatt931 <i.kurnytskyi@gmail.com>
2017-09-15 13:16:46 +0300

    ended project

--- app/js/common.js
+++ app/js/common.js
@@ -2,8 +2,9 @@ $(function() {
 
 	new WOW().init();
 
-		$(window).on("scroll", function (e) {
+		$(window).on("scroll", function scrollingFunction(e) {
 			var scroled = $(window).scrollTop();
+
 
 			$("#first-block").css("left", (0 - (scroled*0.1)) + "%");
 			$("#second-block").css("left", (70 - (scroled*0.1)) + "%");
@@ -11,9 +12,20 @@ $(function() {
 			$("#fourth-block").css("left", (190 - (scroled*0.1)) + "%");
 
 			$("body").css("background-position-x",(100 + (scroled * 0.25)) + "%");
-			//$("#super_bg").css("left", (700 + (scroled*0.25)) + "px");
-			//$("#super_bg").css("top", (850 -(scroled * 0.3)) + "px");
-			//$("#batman_bg").css("top", (1950 -(scroled * 0.1)) + "px");
 		});
+
+	var scroll = [0,700,1200,1950];
+	var i = 0;
+	$('#next').on('click', function() {
+			scroll[i];
+			i++;
+			if (i>3) {
+				i = 0;
+			}
+			$('html, body').animate({
+				scrollTop: scroll[i],
+			}, 1000);
+	});
+
 
 });
