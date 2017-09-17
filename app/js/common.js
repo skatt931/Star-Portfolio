d9f7e647ed2d2b2a7a5fb294b5d5972a1c12060d
skatt931 <i.kurnytskyi@gmail.com>
2017-09-17 20:55:40 +0300

    improve code. now it's become more moduletive. add new block with experience.

--- app/js/common.js
+++ app/js/common.js
@@ -2,24 +2,31 @@ $(function() {
 
 	new WOW().init();
 
-		$(window).on("scroll", function scrollingFunction(e) {
+	$(window).on("scroll", function scrollingFunction(e) {
 			var scroled = $(window).scrollTop();
 
 
 			$("#first-block").css("left", (0 - (scroled*0.1)) + "%");
 			$("#second-block").css("left", (70 - (scroled*0.1)) + "%");
 			$("#third-block").css("left", (120 - (scroled*0.1)) + "%");
-			$("#fourth-block").css("left", (190 - (scroled*0.1)) + "%");
+			$("#fourth-block").css("left", (205 - (scroled*0.1)) + "%");
 
 			$("body").css("background-position-x",(100 + (scroled * 0.25)) + "%");
 		});
 
-	var scroll = [0,700,1200,1950];
+
+
+	//var scroll = [0,680,1360,2040];
+	var scroll = [];
+	$('.animation-block').each(function (i, el) {
+		scroll.push($(this).position().top);
+	})
+	console.log(scroll);
 	var i = 0;
 	$('#next').on('click', function() {
 			scroll[i];
 			i++;
-			if (i>3) {
+			if (i>scroll.length - 1) {
 				i = 0;
 			}
 			$('html, body').animate({
