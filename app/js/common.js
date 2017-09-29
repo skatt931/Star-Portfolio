781f4274e567132e470a7aa6c56c9e86c66188a0
skatt931 <i.kurnytskyi@gmail.com>
2017-09-29 15:31:59 +0300

    change design. add language json.

--- app/js/common.js
+++ app/js/common.js
@@ -1,56 +1,73 @@
 $(function() {
+    (function(){
+        new WOW().init();
 
-	new WOW().init();
+        if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+            return
+        } else {
 
-	if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-		return
-	} else {
-
-		$(window).on("scroll", function scrollingFunction(e) {
-			var scroled = $(window).scrollTop();
+            $(window).on("scroll", function scrollingFunction(e) {
+                var scroled = $(window).scrollTop();
 
 
-			$("#first-block").css("left", (0 - (scroled*0.1)) + "%");
-			$("#second-block").css("left", (70 - (scroled*0.1)) + "%");
-			$("#third-block").css("left", (120 - (scroled*0.1)) + "%");
-			$("#fourth-block").css("left", (205 - (scroled*0.1)) + "%");
-			$("#fifth-block").css("left", (270 - (scroled*0.1)) + "%");
+                $("#first-block").css("left", (0 - (scroled*0.1)) + "%");
+                $("#second-block").css("left", (70 - (scroled*0.1)) + "%");
+                $("#third-block").css("left", (120 - (scroled*0.1)) + "%");
+                $("#fourth-block").css("left", (205 - (scroled*0.1)) + "%");
+                $("#fifth-block").css("left", (270 - (scroled*0.1)) + "%");
 
-			$("body").css("background-position-x",(100 + (scroled * 0.25)) + "%");
-		});
+                $("body").css("background-position-x",(100 + (scroled * 49)) + "%");
+            });
 
-		//var scroll = [0,680,1360,2040];
-		var scroll = [];
-		$('.animation-block').each(function (i, el) {
-			scroll.push($(this).position().top);
-		})
-		console.log(scroll);
-		var i = 0;
-		function moveNext() {
-			scroll[i];
-			i++;
-			if (i>scroll.length - 1) {
-				i = 0;
-			}
-			$('html, body').animate({
-				scrollTop: scroll[i],
-			}, 1000);
-		}
-		$('#next').on('click',moveNext ).hover( function() {
-			$(this).animate({
-                width: '90px',
-                height: '38px'
+            //var scroll = [0,680,1360,2040];
+            var scroll = [];
+            $('.animation-block').each(function (i, el) {
+                scroll.push($(this).position().top);
             })
-		}, function() {
-            $(this).animate({
-                width: '84px',
-                height: '34px'
+            console.log(scroll);
+            var i = 0;
+            function moveNext() {
+                scroll[i];
+                i++;
+                if (i>scroll.length - 1) {
+                    i = 0;
+                }
+                $('html, body').animate({
+                    scrollTop: scroll[i],
+                }, 1000);
+            }
+            $('#next').on('click',moveNext ).hover( function() {
+                $(this).animate({
+                    width: '90px',
+                    height: '38px'
+                })
+            }, function() {
+                $(this).animate({
+                    width: '65px',
+                    height: '34px'
+                })
+            });
+        }
+
+        //TRANSLATION
+        let [$name, $tecnologiesText, $howLongText, $howGoodText, $aboutMeText, $myWorksText, $myContactsText] =
+            [$('.name'), $('.tecnoligies-text'), $('.how-long-text'), $('.how-good-text'), $('.about-me-text'),
+                $('.my-works-text'), $('.my-contacts-text')];
+
+        function translateText() {
+            let _this = $(this);
+
+            $.get('../language/lang.json', function(data) {
+                console.log(_this);
+                console.log(data.eng.name);
+
+                $name.text(data.eng.name);
             })
-        });
-		//$(window).keypress(function(){
-		//	if(event.which == 32) {
-		//		moveNext();
-		//	}
-		//})
-	}
+        }
+
+        $('#language-list').find('a').on('click', translateText);
+
+        translateText();
+
+    })()
 });
